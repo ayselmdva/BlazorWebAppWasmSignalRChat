5e28c8ab5efcb527c649d98e35e5f1d81a9e6bcf
ayselmdva <maysel353@gmail.com>
2024-06-07 23:24:41 +0400

    SignalRChat

---
+++ DemoBlazorWebAppWasmSignalRChat/ChatHubs/ChatHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Data;
+
+namespace DemoBlazorWebAppWasmSignalRChat.ChatHubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string userName, string message, DateTime date)=>await Clients.All.SendAsync("ReceiveMessage", userName, message, date);
+    }
+}
